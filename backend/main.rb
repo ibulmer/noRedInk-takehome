6a20fbe8d5413526ece33dd4fb77f65be0f6d815
Ian <ian.bulmer89@gmail.com>
2016-03-11 11:47:24 -0800

    added helper functions to group arrays and to divide items into groups

--- backend/main.rb
+++ backend/main.rb
@@ -15,6 +15,54 @@ until number.is_a?(Fixnum) && number>0 do
   number = Integer(gets) rescue nil
 end
 
+# function to sort an array of arrays based on a property. Will be used to divide data in Strands and Standards.
+# Index is the index of the property we are sorting on
+def groupByProp(arr, index)
+  # create an object. Keys are a property. Values are an array of arrays.
+  obj = {
+  }
+  # go through inner arrays grouping them into arrays based on property
+
+  arr.each do |inner|
+    key = inner[index]
+    #check if key exists
+    if (obj[key]!=nil)
+      #if so push current inner array to the corresponding object[key] array
+      obj[key].push(inner)
+    else
+      #else make a new key value pair
+      obj[key] = [inner]
+    end
+  end
+  #transform the object into an array of arrays
+  sortedArr = []
+  obj.each do |key, array|
+    sortedArr.push(obj[key])
+  end
+  #return the sorted array
+  return sortedArr
+end
+
+#function to divide items into integer groups of similar size. Used to divide questions between strands and standards.
+def equalSizeGroups(items, groups)
+  #create an array to store the size of each group
+  result = []
+  #calculate how large each group should be
+  size = items/groups
+  #calculate the remainder of dividing items by groups. The excess items must be put in a group
+  remainder = items%groups
+  #create the groups
+  for i in 1..groups
+    temp = size
+    if (remainder>0)
+      temp+=1
+      remainder-=1
+    end
+    result.push(temp)
+  end
+  return result
+end
+
 #function to choose a random element in an array. used to choose a random question.
 def randomElm(arr)
   return arr[rand(arr.length)]
@@ -27,5 +75,4 @@ results = []
 for i in 1..number
   results.push randomElm(questions)[4]
 end
-
 puts results
